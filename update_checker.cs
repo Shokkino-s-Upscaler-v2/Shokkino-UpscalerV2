248249aa02be1366286270e8115204b4aa053d14
ImElio <87428094+ImElio@users.noreply.github.com>
2025-01-30 05:38:12 +0100

    Create update_checker.cs

---
+++ update_checker.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string currentVersion = "2025.01.X.001";
+        string latestVersion = await CheckForUpdatesAsync();
+
+        if (latestVersion != currentVersion)
+        {
+            Console.WriteLine($"A new version {latestVersion} is available. Please update!");
+        }
+        else
+        {
+            Console.WriteLine("You are using the latest version.");
+        }
+    }
+
+    static async Task<string> CheckForUpdatesAsync()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync("https://api.fakeapi.com/latest-version");
+            response.EnsureSuccessStatusCode();
+            string latestVersion = await response.Content.ReadAsStringAsync();
+            return latestVersion;
+        }
+    }
+}
